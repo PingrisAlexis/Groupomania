2806e5a21c00908802e1722329ef0c67627ce97d
Alexis <pingrisalexis@gmail.com>
2021-07-26 21:22:33 +0200

    modif message

--- middleware/password.js
+++ middleware/password.js
@@ -1,11 +1,10 @@
 //Import the password schema.
 const passwordSchema = require('../models/Password');
 
-//Verification that the password validates the schema.
+// Verification that the password validates the schema.
 module.exports = (req, res, next) => {
   if (!passwordSchema.validate(req.body.password)) {
-    res.writeHead(400, 'Password required: Minimum length 8, must have 1 uppercase and 1 lowercase letter, at least 2 digits, and should not have spaces.');
-    res.end('Incorrect password format');
+    res.status(400).json({error:'Password required: Minimum length 8, must have 1 uppercase and 1 lowercase letter, at least 2 digits, and should not have spaces.'});  
   } else {
     next();
   }
