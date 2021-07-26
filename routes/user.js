32b1e8cef6c200e4549ee61a2b31d9b21b4f0aea
Alexis <pingrisalexis@gmail.com>
2021-07-26 21:21:50 +0200

    modif comments

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 //Import Express: Use express router.
 const express = require('express');
+//Import authentification .
+const auth = require('../middleware/auth');
 //Call of the express router with the method.
 const router = express.Router();
 
@@ -9,10 +11,12 @@ const userCtrl = require('../controllers/user');
 const passwordValidation = require('../middleware/password');
 
 //Route to create user.
-//Encrypts the user's password of the user, adds the user to the database.
 router.post('/signup', passwordValidation, userCtrl.signup);
 //Route to login user.
-//Checks the identification identification information user's credentials, by returning the userID from the database and a signed signed JSON token.
-// router.post('/login', userCtrl.login);
+router.post('/login', userCtrl.login);
+//Route to delete a user.
+router.delete('/:id',auth, userCtrl.delete);
+//Route to search a user.
+router.get('/:id', userCtrl.search);
 
 module.exports = router;
