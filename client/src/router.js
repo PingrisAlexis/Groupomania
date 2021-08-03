8fa438493062f42f95736c9a0ddeb6491921d1fd
Alexis <pingrisalexis@gmail.com>
2021-08-03 09:33:10 +0200

    add routes

--- client/src/router.js
+++ client/src/router.js
@@ -3,15 +3,20 @@ import VueRouter from 'vue-router';
 import Index from './views/Index';
 import Home from './views/Home';
 import MyAccount from './views/MyAccount';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import AddPost from './views/AddPost';
+
+import Login from './components/LoginForm';
+import Signup from './components/SignupForm';
+import PostForm from './components/PostForm';
 import TheHeader from './components/TheHeader';
 import UserAccount from './components/UserAccount';
+import OnePost from './components/OnePost';
+
 Vue.use(VueRouter);
 
 export default new VueRouter({
   linkActiveClass: 'is-active',
- routes: [
+  routes: [
    {
      path:'/',
      name:'Index',
@@ -42,6 +47,21 @@ export default new VueRouter({
      path:'/MyAccount',
      name:'MyAccount',
      component: MyAccount,
-   }
+   },
+   {
+    path:'/AddPost',
+    name:'AddPost',
+    component: AddPost,
+  },
+  {
+    path:'/PostForm',
+    name:'PostForm',
+    component: PostForm,
+  },
+  {
+    path:'/OnePost',
+    name:'OnePost',
+    component: OnePost,
+  },
  ]
 });
