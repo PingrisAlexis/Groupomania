b3e15fd07ee2c87eb5c0b8fe21fd553339049a01
Alexis <pingrisalexis@gmail.com>
2021-08-03 09:34:17 +0200

    add post form

---
+++ client/src/views/AddPost.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <post-form></post-form>
+  </div>
+</template>
+
+<script>
+import TheHeader from '../components/TheHeader.vue';
+import PostForm from "@/components/PostForm.vue";
+
+
+export default {
+  components: {
+    TheHeader,
+    PostForm
+  },
+};
+</script>
