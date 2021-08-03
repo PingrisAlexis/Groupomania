498474bc144ca35fcd2e6f7e594b107e09e5024f
Alexis <pingrisalexis@gmail.com>
2021-08-03 09:27:18 +0200

    add gif, max size

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,20 +5,27 @@ const multer = require('multer');
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
-  'image/png': 'png'
+  'image/png': 'png',
+  'image/gif': 'gif'
 };
 
 // Storage location and file name.
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, 'images');
+      callback(null, "images")
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+      let name = file.originalname.split(".")[0]
+      name = name.split(" ").join("_")
+      const extension = MIME_TYPES[file.mimetype]
+      callback(null, name + Date.now() + "." + extension)
   }
-});
+})
+const maxSize = 1 * 1024 * 1024;
 
+var upload = multer({
+  storage: storage,
+  limits: { fileSize: maxSize }
+}).single('bestand');
 
-module.exports = multer({ storage: storage }).single('image');
+module.exports = multer({storage: storage},{limits: {fileSize: maxSize }} ).single("image") 
