cf0e50323289579b4eed783b3ba40f009434288f
Alexis <pingrisalexis@gmail.com>
2021-08-03 09:26:32 +0200

    add multer

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,8 @@
 const express = require('express');
 //Import authentification .
 const auth = require('../middleware/auth');
+//Import Multer to manage images.
+const multer = require("../middleware/multer");
 //Call of the express router with the method.
 const router = express.Router();
 
@@ -9,11 +11,11 @@ const router = express.Router();
 const postCtrl = require('../controllers/post');
 
 //Route to create a post.
-router.post('/', auth, postCtrl.createOnePost);
+router.post('/', auth, multer, postCtrl.createOnePost);
 //Route to modify a post.
-router.put('/:id', auth, postCtrl.modifyOnePost);
+router.put('/:id', auth, multer, postCtrl.modifyOnePost);
 //Route to delete a post.
-router.delete('/:id', auth, postCtrl.deleteOnePost);
+router.delete('/:id', auth, multer, postCtrl.deleteOnePost);
 //Route to get one post.
 router.get('/:id', auth, postCtrl.getOnePost);
 //Route to get all post.
