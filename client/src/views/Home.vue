a4b690dd50a619a596379abd1670d9723f473c4e
Alexis <pingrisalexis@gmail.com>
2021-08-03 09:32:57 +0200

    add on post component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <the-header></the-header>
+    <one-post></one-post>
   </div>
 </template>
 
 <script>
 import TheHeader from '../components/TheHeader.vue';
+import OnePost from '../components/OnePost.vue';
 
 export default {
   name:'Home',
   components: {
-    TheHeader
+    TheHeader,
+    OnePost
   },
 };
 </script>
