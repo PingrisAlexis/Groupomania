113b2000a57e141901c5aa5e5f3b0e879c585b76
Alexis <pingrisalexis@gmail.com>
2021-08-17 20:33:23 +0200

    Modif middlewares

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,54 +1,63 @@
 //Import the database connection.
-const db = require("../database_connect");
+const db = require("../models");
+
+const Comment = db.comment;
 
 //Middleware to create a comment.
 exports.createOneComment = (req, res, next) => {
-  db.query(`INSERT INTO comments VALUES (NULL,'${req.body.message}','${req.body.postId}','${req.body.userId}')`,
-    (error, result) => {
-      if (error) {
-        res.status(400).json(error);
-      }
-      else {
-        res.status(201).json(result);
-      }
-    });
+  const newComment = {
+    comment: req.body.comment,
+    postId: req.body.postId,
+    userId: req.body.userId,
+    createdAt: req.body.createdAt,
+  };
+  Comment.create(newComment)
+  .then (
+    (data) => {res.send(data)}
+  )
+  .catch (
+    (error) => {res.status(404).send({ error })}
+  );
 };
 
 //Middleware to modify a comment.
 exports.modifyOneComment = (req, res, next) => {
-  db.query(`UPDATE comments SET message = '${req.body.message}', WHERE comments.id = '${req.params.id}'`,
-    (error, result) => {
-      if (error) {
-        res.status(400).json({ error });
-      }
-      else {
-        res.status(200).json(result);
-      }
-    });
+  Comment.findOne({ where: { id: req.params.id } })
+  .then(comment => {
+    let userId = req.headers.authorization.split(' ')[3];
+    if (comment.userId == userId) {
+    Comment.update({ message: req.body.message},{ where: { id: req.params.id } })
+    .then( 
+      () => {res.status(201).json({ message: 'Comment has been modified.' })}
+    )
+    .catch(
+      (error) => {res.status(404).json({ error: error });}
+    )} 
+    else {
+			res.status(403).json({error })
+    }
+  })
+  .catch(error => res.status(500).json({ error }));
 };
 
 //Middleware to delete a comment.
 exports.deleteOneComment = (req, res, next) => {
-  db.query(`DELETE FROM comments WHERE comments.id = ${req.params.id}`,
-    (error, result) => {
-      if (error) {
-        res.status(400).json({ error });
-      }
-      else {
-        res.status(200).json(result);
-      }
-    });
+  Comment.delete({ where: { id: req.params.id } }, 
+    (err, data) => {
+    if(err) {
+      return res.status(400).json({ message: 'Comment has not been deleted' });
+    } 
+    res.status(200).json({ message: 'Comment has been deleted'})
+  })
 };
 
 //Middleware to get all comment.
-exports.getAllcomment = (req, res, next) => {
-  db.query(`SELECT * FROM comments ORDER BY id DESC`,
-    (error, result) => {
-      if (error) {
-        res.status(400).json({ error });
-      }
-      else {
-        res.status(200).json(result);
-      }
-    });
+exports.getAllComments = (req, res, next) => {
+  Comment.findAll({ where: { postId: req.params.id}, order: [['id', 'DESC']]})
+  .then (
+    (comment) => {res.status(200).json(comment)}
+  )
+  .catch (
+    (error) => {res.status(400).json({error})}
+  )
 };
