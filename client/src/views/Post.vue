47f411a1576403e9d6a8b9da1b9ba93c6c49176f
Alexis <pingrisalexis@gmail.com>
2021-08-17 20:33:07 +0200

    Add Post page

---
+++ client/src/views/Post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <one-post></one-post>
+  </div>
+</template>
+
+<script>
+import TheHeader from '../components/TheHeader.vue';
+import OnePost from "../components/OnePost.vue";
+
+
+export default {
+  components: {
+    TheHeader,
+    OnePost
+  },
+};
+</script>
