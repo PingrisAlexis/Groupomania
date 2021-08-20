9f31342e126f99abbc63c5d0b6996d6da19cefdd
Alexis <pingrisalexis@gmail.com>
2021-08-20 15:43:34 +0200

    Edit comments routes

--- routes/comment.js
+++ routes/comment.js
@@ -1,20 +1,20 @@
 //Import Express: Use express router.
-const express = require('express');
+// const express = require('express');
 //Import authentification .
-const auth = require('../middleware/auth');
+// const auth = require('../middleware/auth');
 //Call of the express router with the method.
-const router = express.Router();
+// const router = express.Router();
 
 //Import comments controllers. Functions are associated with the different routes.
-const commentCtrl = require('../controllers/comment');
+// const commentCtrl = require('../controllers/comment');
 
 //Route to create a comment.
-router.post('/', auth, commentCtrl.createOneComment);
+// router.post('/', auth, commentCtrl.createOneComment);
 //Route to modify a comment.
-router.put('/:id', auth, commentCtrl.modifyOneComment);
+// router.put('/:id', auth, commentCtrl.modifyOneComment);
 //Route to delete a comment.
-router.delete('/:id', auth, commentCtrl.deleteOneComment);
+// router.delete('/:id', auth, commentCtrl.deleteOneComment);
 //Route to get all commments.
-router.get('/', auth, commentCtrl.getAllComments);
+// router.get('/', auth, commentCtrl.getAllComments);
 
-module.exports = router;
+// module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -9,7 +9,10 @@ const router = express.Router();
 
 //Import posts controllers. Functions are associated with the different routes.
 const postCtrl = require('../controllers/post');
+//Import comments controllers. Functions are associated with the different routes.
+const commentCtrl = require('../controllers/comment');
 
+//Posts
 //Route to create a post.
 router.post('/', auth, multer, postCtrl.createOnePost);
 // //Route to modify a post.
@@ -20,4 +23,14 @@ router.post('/', auth, multer, postCtrl.createOnePost);
 router.get('/:id', auth, postCtrl.getOnePost);
 // //Route to get all posts.
 router.get('/', auth, postCtrl.getAllPosts);
+
+//Comments
+//Route to create a comment.
+router.post('/:id/comments', auth, commentCtrl.createOneComment);
+//Route to modify a comment.
+// router.put('/:id/comments/:id', auth, commentCtrl.modifyOneComment);
+//Route to delete a comment.
+// router.delete('/:id/comments/:id', auth, commentCtrl.deleteOneComment);
+//Route to get all commments.
+router.get('/:id/comments', auth, commentCtrl.getAllComments);
 module.exports = router;
