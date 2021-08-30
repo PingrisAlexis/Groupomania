945def9b6c8dfd9b46124ffc305e42ceaa5d4089
Alexis <pingrisalexis@gmail.com>
2021-08-30 16:04:12 +0200

    add modify one post

--- controllers/post.js
+++ controllers/post.js
@@ -47,32 +47,37 @@ exports.getOnePost = (req, res, next) => {
 
 //Middleware to modify a post.
 exports.modifyOnePost = (req, res, next) => {
-  Post.findOne({ where: { id: req.params.id } })
-  .then(post => {
-      let filename = post.media.split("/images/")[1];
-      fs.unlink("images/"+ filename, () => {
-        Post.update({ title: req.body.title, message: req.body.message, media: req.protocol + "://" + req.get("host") + "/images/" + req.file.filename},
-        { where: { id: req.params.id } })
-          .then( 
-            () => {res.status(201).json({ message: "Media is update." })}
-          )
-          .catch (
-            (error) => {res.status(404).json({ error })}
-          )
-        })
+  Post.findOne({where: { id: req.params.id} })
+  .then (post => {
+    let img = post.media.split('/images/')[1];
+    const postObject = {
+      title: req.body.title ? req.body.title : post.title,
+      message: req.body.message ? req.body.message : post.message,
+      media: req.file ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}` : post.media
+    }
+    if (req.file ) {
+      fs.unlink("images/"+ img, () => {
+      Post.update({ ...postObject, id:  req.params.id}, { where: { id: req.params.id }})
+        .then(() => res.status(200).json({ message: "Post has been modified" }))
+        .catch(error => res.status(400).json({ error }))
+      })}
+    else {
+      Post.update({ ...postObject, id:  req.params.id}, { where: { id: req.params.id }})
+        .then(() => res.status(200).json({ message: "Post has been modified" }))
+        .catch(error => res.status(400).json({ error }))
+    }
   })
-  .catch ( 
+  .catch (
     (error) => res.status(500).json({ error })
   );
-};
+}
 
 //Middleware to delete a post.
 exports.deleteOnePost = (req, res, next) => {
   Post.findOne({ where: { id: req.params.id } })
-    .then (post => {
+   .then (post => {
       let filename = post.media.split('/images/')[1];
-      fs.unlink("images/"+ filename, 
-      () => {
+      fs.unlink("images/"+ filename, () => {
         Post.destroy({ where: { id: req.params.id } })
           .then(
             () => res.status(200).json({ message: 'The post has been deleted !' })
