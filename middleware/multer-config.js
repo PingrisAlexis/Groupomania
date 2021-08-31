7ed4b5a852e012bf557e4558463181e7eb9ed097
Alexis <pingrisalexis@gmail.com>
2021-08-31 15:02:29 +0200

    Optimize

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -6,7 +6,6 @@ const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png',
-  'image/gif': 'gif'
 };
 
 // Storage location and file name.
@@ -20,12 +19,8 @@ const storage = multer.diskStorage({
       const extension = MIME_TYPES[file.mimetype]
       callback(null, name + Date.now() + "." + extension)
   }
-})
+});
+
 const maxSize = 1 * 1024 * 1024;
 
-var upload = multer({
-  storage: storage,
-  limits: { fileSize: maxSize }
-}).single('bestand');
-
-module.exports = multer({storage: storage},{limits: {fileSize: maxSize }} ).single("image") 
+module.exports = multer({storage: storage},{limits: {fileSize: maxSize }} ).single("image");
