a79aae93cad6bdce80fcb41eb56ee723d0b77d66
Alexis <pingrisalexis@gmail.com>
2021-09-02 09:38:18 +0200

    Optimize

--- client/src/main.js
+++ client/src/main.js
@@ -6,11 +6,6 @@ import axios from "axios";
 Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 
-if (localStorage.user != undefined) {
-  Vue.prototype.$token = JSON.parse(localStorage.user).token;
-  Vue.prototype.$user = JSON.parse(localStorage.user);
-}
-
 new Vue({
   router,
   render: h => h(App),
--- models/posts.js
+++ models/posts.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     title: {
-      type: DataTypes.STRING(50),
+      type: DataTypes.STRING(30),
       allowNull: false
     },
     message: {
